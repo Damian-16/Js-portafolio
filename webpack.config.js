dfeb9e10c33e451ad43050cc750ed02b1bab8786
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-03-29 01:56:37 -0400

    se agrego babel y htmlwebpack

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+//se añade el recurso html-webpackPlugin
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+ /* Aquí indicamos el elemento inicial de nuestra app.
+    O sea, nuestro punto de entrada */    
+    entry:'./src/index.js',
+    
+/* Ahora indicamos cual va a ser el archivo de salida,
+    donde va a estar todo el código que compile */
+    output:{
+
+         /* Aquí indicamos donde queremos que se guarde el proyecto */
+
+    //aca llamamos path y utilizamos a path de arriba con resolve que nos permite saber en  que directorio esta mi proyecto 
+    //asi cuando enviamos esto a un servidor par apreparar nuestro proyecto va  a utilizar el directorio que esta encontrandose este proyecto
+        path: path.resolve(__dirname,'dist'),//si ponemos 'dist'es el standar del nombre del proyecto
+        /* Y colocamos el nombre del .js que va guardar */
+        filename:'main.js',
+   },
+    /* Vamos a indicar con extensiones vamos a trabajar en
+    este proyecto */
+    resolve:{
+
+         /* Importante pasar las extensiones con las que vamos a
+        trabajar, como .jsx (React.js)  */
+        extensions: ['.js']
+    },
+
+ //un modulo que es un objeto
+    module:{
+      //que internamente utiliza rules para establecer un array donde tenemos estas configuraciones en este caso babel-loader
+        rules:[
+      {//este objeto va a ser para trabajar con babel loader 
+            //y poder conectar nuestro webpack con babel 
+     test:/\.m?js$/,
+     //este test nos va  adecir que tipo de extensiones vamos a utilizar
+     //cualquier archivo que empiece con m o js encuentralo (mjs es la de modulos)
+    exclude:/node_modules/,//esto es para excluir el nodemodules
+
+    use:{
+        loader:'babel-loader',// de esta forma ya tenemos la configuracion necesaria para este recurso
+        }
+     }
+   ]
+ },
+
+ plugins:[
+    //aca se añade el HtmlWebpackPluginq ue fue declarado arriba
+    new HtmlWebpackPlugin({
+        inject:true,
+        template:'./public/index.html',
+        filename:'./index.html',
+    })// como argumento se pone inject para que haga la incersion de html
+    //luego un template que es donde se encuentra
+    //filename x estandar se usa el msimo nombre en este caso html y lo pondra en la carpeta de  distribution
+
+ ]
+
+
+
+}
