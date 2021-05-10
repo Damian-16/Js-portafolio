bab3ff446103b8f8a63607309892e7c1b15eb651
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-05-10 16:26:35 -0400

    se agrego webpackconfig.de.js y su configuracion solo para modo desarrollo y se configuro watch

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import '@styles/main.css';
 import '@styles/vars.styl';
 
 
+// console.log("hello")
+
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
